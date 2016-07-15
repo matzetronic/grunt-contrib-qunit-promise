d67865954a3c0c6d8cc5e35ca6e2dd225ae4804e
Mathias Baumann <mathias.baumann@sap.com>
2016-07-15 10:33:42 +0200

    grunt-contrib-qunit does not work with amd modules
    Tests started via 'grunt' will run through phantomjs and time out.
    Then run 'grunt connect' and open http://localhost:8000/test/index.html to
    see that the tests run successfully.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,31 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		qunit: {
-			files: "test/index.html"
+			all: {
+				options: {
+					urls: ["http://localhost:8000/test/index.html"]
+				}
+			}
+		},
+		connect: {
+			server: {
+				options: {
+				  port: 8000,
+				  base: '.',
+				  keepalive: true
+				}
+			},
+			phantomjs: {
+				options: {
+				  port: 8000,
+				  base: '.'
+				}
+			}
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks("grunt-contrib-qunit");
 
-	grunt.registerTask("default", ["qunit"]);
+	grunt.registerTask("default", ["connect:phantomjs", "qunit"]);
 };
